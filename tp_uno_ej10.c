010d39457349ea9ffa8bd161e94f9a19205733ca
juan-guido <187232029+juan-guido@users.noreply.github.com>
2026-05-12 08:43:57 -0300

    Inicia ejercicio 10 de la guía 1.

---
+++ tp_uno_ej10.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+Una concesionaria de autos desea liquidar el sueldo a cada vendedor pagando $ 500 por mes más un plus del 10% del precio sobre cada vehículo vendido y un valor constante de 50 pesos por cada uno de ellos, se ingresa el valor del vehiculo y cuantos vehiculos dee ese tipo vendio, calcular su sueldo e imprimirlo.
+*/
+
+int main(void){
+    const unsigned int REMUNERACIONFIJA = 500U, PLUSFIJO = 50U;
+    unsigned int cantidadVehiculosVendidos, valorVehiculosVendidos;
+    float sueldoTotal;
+
+    printf
+
+    return 0;
+}
