646486ea313843845be45b885d404a99b40a0355
juan-guido <187232029+juan-guido@users.noreply.github.com>
2026-05-12 08:54:39 -0300

    Completa el ejercicio 10 de la guía 1.

--- tp_uno_ej10.c
+++ tp_uno_ej10.c
@@ -6,10 +6,20 @@ Una concesionaria de autos desea liquidar el sueldo a cada vendedor pagando $ 50
 
 int main(void){
     const unsigned int REMUNERACIONFIJA = 500U, PLUSFIJO = 50U;
-    unsigned int cantidadVehiculosVendidos, valorVehiculosVendidos;
-    float sueldoTotal;
+    unsigned int cantidadVehiculosVendidos;
+    float sueldoTotal, valorVehiculosVendidos, remuneracionVariable;
 
-    printf
+    printf("Ingrese cantidad de vehiculos vendidos por el trabajador: ");
+    scanf("%u", &cantidadVehiculosVendidos);
+
+    printf("Ingrese el valor unitario del vehiculo vendido: ");
+    scanf("%f", &valorVehiculosVendidos);
+
+    remuneracionVariable = (cantidadVehiculosVendidos * valorVehiculosVendidos) * 0.10 + cantidadVehiculosVendidos * PLUSFIJO;
+
+    sueldoTotal = remuneracionVariable + REMUNERACIONFIJA;
+
+    printf("El sueldo del trabajador por vender %u es de $ %.2f.", cantidadVehiculosVendidos, sueldoTotal);
 
     return 0;
 }
