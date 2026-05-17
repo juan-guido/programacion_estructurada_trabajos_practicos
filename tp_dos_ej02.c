34b7f8edf2919f298faed591bcb86b8781749879
juan-guido <187232029+juan-guido@users.noreply.github.com>
2026-05-17 18:27:55 -0300

    Corrije errores del ejercicio 2 de la guía 2.

--- tp_dos_ej02.c
+++ tp_dos_ej02.c
@@ -4,25 +4,25 @@
 Ingresar un valor indicar e imprimir si es positivo, negativo o cero
 */
 
-int main(){
+int main(void){
 
     int numero_ingresado;
     numero_ingresado = 0;
 
     printf("Ingrese un numero para determinar si es positivo, negativo o cero: \n");
-    scanf("%i", &numero_ingresado);
+    scanf("%d", &numero_ingresado);
 
     if (numero_ingresado > 0) {
 
-        printf("El numero %i es positivo.", numero_ingresado);
+        printf("El numero %d es positivo.", numero_ingresado);
     }
     else if (numero_ingresado < 0) {
 
-        printf("El numero %i es negativo.", numero_ingresado);
+        printf("El numero %d es negativo.", numero_ingresado);
     }
     else {
 
-        printf("El numero %i es efectivamente cero.", numero_ingresado);
+        printf("El numero %d es efectivamente cero.", numero_ingresado);
     }
     return 0;
 }
