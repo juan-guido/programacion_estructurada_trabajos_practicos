e714a2126762e4e57952237955ed074f24dba0ec
juan-guido <187232029+juan-guido@users.noreply.github.com>
2026-05-17 19:04:31 -0300

    Completa el ejercicio 1 de la guía 2.

--- tp_dos_ej01.c
+++ tp_dos_ej01.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 
+/*
+Ingresar dos valores, indicar e imprimir si son iguales.
+*/
+
+
 int main(void){
 
+    int primer_numero_ingresado;
+    int segundo_numero_ingresado;
+
+    printf("Ingrese el primer número para ser comparado: \n>>> ");
+    scanf("%d", &primer_numero_ingresado);
+
+    printf("Ingrese el segundo número para ser comparado: \n>>> ");
+    scanf("%d", &segundo_numero_ingresado);
+
+    if (primer_numero_ingresado==segundo_numero_ingresado){
+        printf("Los numeros %d y %d son iguales.", primer_numero_ingresado, segundo_numero_ingresado);
+    }
 
     return 0;
 }
