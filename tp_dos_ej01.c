d3713de2e16947f5221d5e755055eb32f7721b5f
juan-guido <187232029+juan-guido@users.noreply.github.com>
2026-05-17 19:07:06 -0300

    Corrije errores del ejercicio 1 de la guía 2.

--- tp_dos_ej01.c
+++ tp_dos_ej01.c
@@ -16,7 +16,7 @@ int main(void){
     printf("Ingrese el segundo número para ser comparado: \n>>> ");
     scanf("%d", &segundo_numero_ingresado);
 
-    if (primer_numero_ingresado==segundo_numero_ingresado){
+    if (primer_numero_ingresado == segundo_numero_ingresado){
         printf("Los numeros %d y %d son iguales.", primer_numero_ingresado, segundo_numero_ingresado);
     }
 
