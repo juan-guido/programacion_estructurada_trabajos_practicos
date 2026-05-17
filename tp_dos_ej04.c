40f56af85f5e2d6d13b0639e59b95f9fef07d312
juan-guido <187232029+juan-guido@users.noreply.github.com>
2026-05-17 19:51:15 -0300

    Inicial el ejercicio 4 de la guía 2.

---
+++ tp_dos_ej04.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+Ingresar tres valores, sumarlos, calcular el promedio e indicar e imprimir cuál de estos valores es mayor al promedio.
+*/
+
+int main (void) {
+
+
+    
+    return 0;
+}
