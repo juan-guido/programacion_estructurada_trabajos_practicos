17f6dafb3d44473e0cfe7eedbe26714712444692
juan-guido <187232029+juan-guido@users.noreply.github.com>
2026-05-17 23:19:37 -0300

    Corrije errores del ejercicio 6 de la guía 2.

--- tp_dos_ej06.c
+++ tp_dos_ej06.c
@@ -7,26 +7,32 @@ Ingresar la edad y la altura de dos personas, indicar e imprimir la estatura del
 int main (void){
     unsigned char edad_ingresada_a;
     unsigned char edad_ingresada_b;
-    float estatura_persona_mas_mayor_metros;
+    float estatura_persona_a_metros;
+    float estatura_persona_b_metros;
 
     printf("Ingrese la edad de la persona A: \n>>> ");
     scanf("%hhu", &edad_ingresada_a);
 
+    printf("Ingrese la altura de la persona A: \n>>> ");
+    scanf("%f", &estatura_persona_a_metros);
+
     printf("Ingrese la edad de la persona B: \n>>> ");
     scanf("%hhu", &edad_ingresada_b);
 
-    if (edad_ingresada_a >= edad_ingresada_b) {
+    printf("Ingrese la altura de la persona B: \n>>> ");
+    scanf("%f", &estatura_persona_b_metros);
 
-        printf("Ingrese la altura de la persona A: \n>>> "),
-        scanf("%f", &estatura_persona_mas_mayor_metros);
-        
-        printf("La persona A de %hhu años, mide %.2f metros.", edad_ingresada_a, estatura_persona_mas_mayor_metros);
-    } else {
+    if (edad_ingresada_a > edad_ingresada_b) {
 
-        printf("Ingrese la altura de la persona B: \n>>> "),
-        scanf("%f", &estatura_persona_mas_mayor_metros);
+        printf("La persona A de %hhu años, mide %.2f metros.", edad_ingresada_a, estatura_persona_a_metros);
+    } 
+    else if (edad_ingresada_a < edad_ingresada_b) {
         
-        printf("La persona B de %hhu años, mide %.2f metros.", edad_ingresada_b, estatura_persona_mas_mayor_metros);
+        printf("La persona B de %hhu años, mide %.2f metros.", edad_ingresada_b, estatura_persona_b_metros);
+    } 
+    else {
+
+        printf("Ambas personas tienen la misma edad.");
     }
 
     return 0;
