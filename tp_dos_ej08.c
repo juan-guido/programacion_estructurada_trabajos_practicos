d854f04a028789e964771dc45b1423055de408bf
juan-guido <187232029+juan-guido@users.noreply.github.com>
2026-05-18 08:09:16 -0300

    Agrega el ejercicio 8 de la guía 2.

---
+++ tp_dos_ej08.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*
+Ingresar tres valores correspondientes al día, mes y año de una fecha, indicar si es válida, considerar los años bisiestos.
+*/
+
+int main (void) {
+
+
+    return 0;
+}
