78315a059db21790190ecdd7b4b917e5e9a44a09
juan-guido <187232029+juan-guido@users.noreply.github.com>
2026-05-18 08:43:01 -0300

    Corrije errores del ejercicio 8 de la guía 2.

--- tp_dos_ej08.c
+++ tp_dos_ej08.c
@@ -1,11 +1,76 @@
 #include <stdio.h>
 
 /*
-Ingresar tres valores correspondientes al día, mes y año de una fecha, indicar si es válida, considerar los años bisiestos.
+Ingresar tres valores correspondientes al día, mes__ingresado y año de una fecha, indicar si es válida, considerar los años bisiestos.
 */
 
 int main (void) {
 
+    unsigned char dia_ingresado;
+    unsigned char mes__ingresado;
+    int ano_ingresado;  
+
+    printf("Ingrese un año: \n>>> ");
+    scanf("%d", &ano_ingresado);
+
+    printf("Ingrese un mes: \n>>> ");
+    scanf("%hhu", &mes__ingresado);
+
+    printf("Ingrese un día: \n>>> ");
+    scanf("%hhu", &dia_ingresado);
+
+    if (mes__ingresado == 1 || mes__ingresado == 3 || mes__ingresado == 5 || mes__ingresado == 7 || mes__ingresado == 8 || mes__ingresado == 10 || mes__ingresado == 12) {
+
+        if (dia_ingresado <= 31 && dia_ingresado > 0) {
+
+            printf("La fecha es valida.");
+        }
+        else {
+
+            printf("La fecha es invalida.");
+        }
+    }
+    else if (mes__ingresado == 4 || mes__ingresado == 6 || mes__ingresado == 9 || mes__ingresado == 11) {
+
+        if (dia_ingresado <= 30 && dia_ingresado > 0) {
+
+            printf("La fecha es valida.");
+        }
+        else {
+
+            printf("La fecha es invalida.");
+        }        
+    }
+    else if (mes__ingresado == 2) {
+
+        if ( (ano_ingresado % 4 == 0 && ano_ingresado % 100 != 0) || ano_ingresado % 400 == 0 )  {
+
+            if (dia_ingresado <= 29 && dia_ingresado > 0) {
+
+                printf("La fecha es valida.");
+            }
+            else {
+
+                printf("La fecha es invalida.");
+            }
+        }
+        else {
+
+            if (dia_ingresado <= 28 && dia_ingresado > 0) {
+
+                printf("La fecha es valida.");
+            }
+            else {
+
+                printf("La fecha es invalida.");
+            }
+
+        }
+    }
+    else {
+
+        printf("La fecha es invalida.");
+    }
 
     return 0;
 }
